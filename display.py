95258ba48c605d2547154eea8dc69bbff3fc21fe
Vision295 <theophilewacthel@gmail.com>
2025-01-31 08:15:58 +0100

    trois fois rien

--- display.py
+++ display.py
@@ -1,11 +1,30 @@
 import pygame
+from multiprocessing import Queue
 
 
 class Display():
       
       
-      def __init__(self):
+      def __init__(self, queues:list[Queue]):
             self.running = True
             self.screen = pygame.display.set_mode((700, 700))
 
+
+            self.clock = pygame.time.Clock()
+            self.fps = 20
+            
+            
+      def run(self):
+            
+            while self.running:
+                  
+                  
+                  
+                  pygame.display.flip()
+                  
+                  for even in pygame.event.get():
+                        if even.type == pygame.QUIT:
+                              pygame.quit()
+                              running = False
+      
 
